f91e536ba999fe4f9cb3cbd70e4982fb8a84b605
Mikhail Lipin <v-milipi@microsoft.com>
2020-02-21 18:39:19 +0300

    Adding unit tests.

---
+++ src/System.Windows.Forms/tests/UnitTests/PropertyGridInternal/PropertyGridViewTests.cs
@@ -0,0 +1,30 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Xunit;
+
+namespace System.Windows.Forms.Tests.PropertyGridInternal
+{
+    public class PropertyGridViewTests
+    {
+        [Fact]
+        public void PropertyGridView_Constructor()
+        {
+            var propertyGrid = new PropertyGrid();
+            var entity = new Entity();
+            propertyGrid.SelectedObject = entity;
+
+            var item = propertyGrid.SelectedGridItem;
+
+            Assert.NotNull(item);
+
+            Assert.NotNull(item.Value);
+        }
+    }
+
+    public class Entity
+    {
+        string[] Values { get; set; }
+    }
+}
